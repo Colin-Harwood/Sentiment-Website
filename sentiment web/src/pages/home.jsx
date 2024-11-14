c7f1344824f6e2320584f72483784a7695ad9381
Colin Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-11-14 21:51:57 +0200

    Added the text input field and the submit button

--- sentiment web/src/pages/home.jsx
+++ sentiment web/src/pages/home.jsx
@@ -7,6 +7,15 @@ const Home = () => {
       <h1>
         Hi
       </h1>
+      <div id="formDiv">
+        <form>
+          <input type="text" placeholder='Enter what you want analysised' id='formInput'>
+          </input>
+          <button type='submit' id='formSubmit'>
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
